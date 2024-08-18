070ac9c0cef0f0a94f54bb98b4e70fe4c8ec141e
Michael Nascimento <michael.nsc@outlook.com>
2024-08-18 00:43:21 -0300

    Update index.test.js

--- calculadora/index.test.js
+++ calculadora/index.test.js
@@ -1,13 +1,11 @@
+import assert from "node:assert";
+import { it } from "node:test";
+import { add, subtract } from "./index.js";
 
-import assert from 'node:assert'
-import { it } from 'node:test'
-import { add } from './index.js'
+it("should sum 1 + 1 equals 2", () => {
+  assert.equal(add(1, 1), 2);
+});
 
-
-it('should return true', (t) => {
-  assert.equal(1, 1)
-})
-
-it('should sum 1 + 1 equals 2', ()=> {
-  assert.equal(add(1,1), 1)
-})
+it("should subtract 1 - 1 equals 0", () => {
+  assert.equal(subtract(1, 1), 0);
+});
